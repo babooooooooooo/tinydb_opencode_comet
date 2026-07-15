e7e62a105ba19e8f687003a00a0617e17219be78
刘朕 <1303924819@qq.com>
2026-07-15 16:46:59 +0800

    feat: expose new modules via tinydb public API

---
+++ tinydb/cli/__init__.py
@@ -0,0 +1,4 @@
+# tinydb/cli/__init__.py
+from tinydb.cli.repl import REPL
+
+__all__ = ["REPL"]
---
+++ tinydb/sql/__init__.py
@@ -0,0 +1,4 @@
+# tinydb/sql/__init__.py
+from tinydb.sql.executor import IndexScanOperator, Planner
+
+__all__ = ["IndexScanOperator", "Planner"]
