5dc2fee9a6da3664728e84b3172c16fba73dc20a
刘朕 <1303924819@qq.com>
2026-07-15 16:30:39 +0800

    feat(sql): update package exports

--- tinydb/sql/__init__.py
+++ tinydb/sql/__init__.py
@@ -1,1 +1,22 @@
 """tinydb SQL engine package."""
+from tinydb.sql.database import Database
+from tinydb.sql.result import QueryResult
+from tinydb.sql.errors import (
+    SQLError,
+    LexerError,
+    ParserError,
+    PlanningError,
+    ExecutionError,
+    ConstraintError,
+)
+
+__all__ = [
+    "Database",
+    "QueryResult",
+    "SQLError",
+    "LexerError",
+    "ParserError",
+    "PlanningError",
+    "ExecutionError",
+    "ConstraintError",
+]
